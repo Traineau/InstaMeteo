fb092e40e02aa21d8e2c91617e5bd401cb774544
emericmottier <emericmottier72@gmail.com>
2018-04-04 11:43:05 +0200

    ajout icone

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
         <link href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
         <link href='https://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
         <link href="css/main.css" rel="stylesheet">
-
+		<link rel="icon" type="images/png" href="images/icone.png" />
         <script src="js/jquery-3.3.1.js"></script>
         <script src="js/jquery-ui.min.js"></script>
         <script src="js/bootstrap.bundle.min.js"></script>
